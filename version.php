5bf4fe1378970c7d01fa4363ac8db44da78e0e57
Georg Maißer <georg.maisser@wunderbyte.at>
2024-04-09 10:52:45 +0200

    Version (2024040900)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023092600;  // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2024040900;  // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2017111300; // M3.4+.
 $plugin->cron = 300;
 $plugin->component = 'auth_campusconnect'; // Full name of the plugin (used for diagnostics).
